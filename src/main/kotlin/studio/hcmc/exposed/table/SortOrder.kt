508fbab877ea2679b8afc4e2b5f4c32d7d966d26
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-12 00:40:57 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/exposed/table/SortOrder.kt
+++ src/main/kotlin/studio/hcmc/exposed/table/SortOrder.kt
@@ -1,16 +1,38 @@
 package studio.hcmc.exposed.table
 
-import org.jetbrains.exposed.sql.ExpressionWithColumnType
-import org.jetbrains.exposed.sql.Op
-import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.wrap
 
-fun <T : Comparable<T>, S : T?> SortOrder.buildOp(expression: ExpressionWithColumnType<in S>, value: T): Op<Boolean> {
+fun <T : Comparable<T>, S : T?> SortOrder.buildOp(expression: ExpressionWithColumnType<in S>, value: T): ComparisonOp {
     return when (this) {
         SortOrder.ASC,
         SortOrder.ASC_NULLS_FIRST,
-        SortOrder.ASC_NULLS_LAST -> Op.build { expression greater value }
+        SortOrder.ASC_NULLS_LAST -> GreaterOp(expression, expression.wrap(value))
         SortOrder.DESC,
         SortOrder.DESC_NULLS_FIRST,
-        SortOrder.DESC_NULLS_LAST -> Op.build { expression less value }
+        SortOrder.DESC_NULLS_LAST -> LessOp(expression, expression.wrap(value))
+    }
+}
+
+fun <T : Comparable<T>, S : T?> SortOrder.buildOp(expression: Expression<in S>, other: Expression<in S>): ComparisonOp {
+    return when (this) {
+        SortOrder.ASC,
+        SortOrder.ASC_NULLS_FIRST,
+        SortOrder.ASC_NULLS_LAST -> GreaterOp(expression, other)
+        SortOrder.DESC,
+        SortOrder.DESC_NULLS_FIRST,
+        SortOrder.DESC_NULLS_LAST -> LessOp(expression, other)
+    }
+}
+
+fun <T : Comparable<T>> SortOrder.buildOp(expression: ExpressionWithColumnType<EntityID<T>>, value: T): ComparisonOp {
+    return when (this) {
+        SortOrder.ASC,
+        SortOrder.ASC_NULLS_FIRST,
+        SortOrder.ASC_NULLS_LAST -> GreaterOp(expression, expression.wrap(value))
+        SortOrder.DESC,
+        SortOrder.DESC_NULLS_FIRST,
+        SortOrder.DESC_NULLS_LAST -> LessOp(expression, expression.wrap(value))
     }
 }
