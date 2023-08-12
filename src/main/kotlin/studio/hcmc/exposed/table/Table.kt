f8178ca68eb2c6728f0110f96b3f7f7e4d60f2af
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-12 22:21:19 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/exposed/table/Table.kt
+++ src/main/kotlin/studio/hcmc/exposed/table/Table.kt
@@ -1,6 +1,7 @@
 package studio.hcmc.exposed.table
 
 import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.*
 import studio.hcmc.exposed.transaction.blockingTransaction
 import studio.hcmc.exposed.transaction.suspendedTransaction
@@ -21,6 +22,21 @@ fun <T : Comparable<T>, E : EntityID<T>> Table.reference(
         .apply(configure)
 }
 
+@Suppress("UNCHECKED_CAST")
+fun <T : Comparable<T>, E : EntityID<T>> Table.referenceEntity(
+    refColumn: Column<E>,
+    onDelete: ReferenceOption? = null,
+    onUpdate: ReferenceOption? = null,
+    fkName: String? = null,
+    configure: Column<E>.() -> Unit = {}
+): ReadOnlyProperty<Table, Column<E>> = ReadOnlyProperty { table, property ->
+    val name = property.name
+    ColumnDelegate.findPresent(table, name) ?: table
+        .let { entityId(name, (refColumn.columnType as EntityIDColumnType<T>).idColumn) as Column<E> }
+        .apply { references(refColumn, onDelete, onUpdate, fkName) }
+        .apply(configure)
+}
+
 fun <T : Comparable<T>, E : EntityID<T>> Table.optReference(
     refColumn: Column<E>,
     onDelete: ReferenceOption? = null,
@@ -29,10 +45,26 @@ fun <T : Comparable<T>, E : EntityID<T>> Table.optReference(
     configure: Column<T?>.() -> Unit = {}
 ): ReadOnlyProperty<Table, Column<T?>> = ReadOnlyProperty { table, property ->
     val name = property.name
-    NullableColumnDelegate.findPresent<T>(table, name) ?: table
+    NullableColumnDelegate.findPresent(table, name) ?: table
         .registerColumn<T>(name, refColumn.columnType.rawSqlType())
         .nullable()
         .apply { foreignKey = ForeignKeyConstraint(refColumn, this, onUpdate, onDelete, fkName) }
+        .apply(configure)
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Comparable<T>, E : EntityID<T>> Table.optReferenceEntity(
+    refColumn: Column<E>,
+    onDelete: ReferenceOption? = null,
+    onUpdate: ReferenceOption? = null,
+    fkName: String? = null,
+    configure: Column<E?>.() -> Unit = {}
+): ReadOnlyProperty<Table, Column<E?>> = ReadOnlyProperty { table, property ->
+    val name = property.name
+    NullableColumnDelegate.findPresent(table, name) ?: table
+        .let { entityId(name, (refColumn.columnType as EntityIDColumnType<T>).idColumn) as Column<E> }
+        .nullable()
+        .apply { reference(refColumn, onDelete, onUpdate, fkName) }
         .apply(configure)
 }
 
