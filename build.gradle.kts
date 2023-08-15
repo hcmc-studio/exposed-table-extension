53c99e8472466c5f17a9d1c885a1214755d49a62
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-15 17:48:22 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,13 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "studio.hcmc"
-            artifactId = "exposed-table-extension"
+            artifactId = project.name
+            version = project_version
+            from(components["java"])
+        }
+        create<MavenPublication>("jitpack") {
+            groupId = "com.github.hcmc-studio"
+            artifactId = project.name
             version = project_version
             from(components["java"])
         }
